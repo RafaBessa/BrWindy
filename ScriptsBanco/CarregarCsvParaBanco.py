1ebfc03327d141f2b10c4fec8a75f2c419715e45
Rafael Bessa <rafablou@gmail.com>
2019-10-30 11:51:36 -0300

    Script para passar todos os csvs pro banco

--- ScriptsBanco/CarregarCsvParaBanco.py
+++ ScriptsBanco/CarregarCsvParaBanco.py
@@ -1,8 +1,10 @@
 import csv
 from pymongo import MongoClient
 from pprint import pprint
+import os
 def CsvToDict(pathArquivo,NomeBoia):
-   
+    
+
 
     arquivo = open(pathArquivo)
 
@@ -20,8 +22,6 @@ def CsvToDict(pathArquivo,NomeBoia):
     Dado = { '# Epoca':'',
             'Date':{}, 
             'Location':{},
-            'Battery':'',
-            'bHead':'',
             'Wspd':'',
             'Wspdflag':'',
             'Wdir': '',
@@ -32,25 +32,48 @@ def CsvToDict(pathArquivo,NomeBoia):
             'Humi':'',
             'Arad':'',
             'Wtmp':'',
-            'Cvel1':'',
-            'Cdir1':'',
-            'Cvel2':'',
-            'Cdir2':'',
-            'Cvel3':'',
-            'Cdir3':'',
             'Wvht':'',
             'Wmax':'',
             'Dpd':'',
             'Mwd':'',
             'Spred':''
             }
+
+            #  Dado = { '# Epoca':'',
+            # 'Date':{}, 
+            # 'Location':{},
+            # 'Battery':'',
+            # 'bHead':'',
+            # 'Wspd':'',
+            # 'Wspdflag':'',
+            # 'Wdir': '',
+            # 'Gust':'',
+            # 'Atmp':'',  
+            # 'Pres':'',
+            # 'Dewp':'',
+            # 'Humi':'',
+            # 'Arad':'',
+            # 'Wtmp':'',
+            # 'Cvel1':'',
+            # 'Cdir1':'',
+            # 'Cvel2':'',
+            # 'Cdir2':'',
+            # 'Cvel3':'',
+            # 'Cdir3':'',
+            # 'Wvht':'',
+            # 'Wmax':'',
+            # 'Dpd':'',
+            # 'Mwd':'',
+            # 'Spred':''
+            # }
     Dado = {}
-    NomeDosDados = ['# Epoca','Battery','bHead','Wspd','Wspdflag','Wdir','Gust','Atmp', 
-                    'Pres','Dewp','Humi','Arad','Wtmp','Cvel1','Cdir1','Cvel2',
-                    'Cdir2','Cvel3','Cdir3','Wvht','Wmax','Dpd','Mwd','Spred']
+    NomeDosDados = ['# Epoca','Wspd','Wspdflag','Wdir','Gust','Atmp', 
+                    'Pres','Dewp','Humi','Arad','Wtmp','Wvht','Wmax','Dpd','Mwd','Spred']
     #transformar o csv em dict
     for d in dados:            
         #Pegar a Data
+        if(d['Year'] != '2016' ):
+            continue
         Date['Year'] = d['Year']
         Date['Month'] = d['Month']
         Date['Day'] = d['Day']
@@ -66,25 +89,42 @@ def CsvToDict(pathArquivo,NomeBoia):
             Dado[s] = d[s]
 
         Boia['Data'].append(Dado)
-        print(Dado)
-        print('-----------------------------------------------------')
+        #print(Dado)
+       # print('-----------------------------------------------------')
         # i+=1
         # if(i>3):
         #     break
-    print(Boia)
-    Boia['Nome'] = NomeBoia
-    connectarbanco(Boia)
+    #print(Boia)
+    Boia['Name'] = NomeBoia
+    print(NomeBoia + " - Completo")
+    return AdicionarBoiaBanco(Boia)
     #enviar o dict para o banco
 
-def connectarbanco(obj):
+def AdicionarBoiaBanco(obj):
     client = MongoClient("mongodb+srv://Bessa:vEiz8yoTtgOZzhyC@brwindy-adtf0.mongodb.net/test?retryWrites=true&w=majority")    
     db=client.admin
     # Issue the serverStatus command and print the results
-    serverStatusResult=db.command("serverStatus")
-    pprint(serverStatusResult)
+  #  serverStatusResult=db.command("serverStatus")
+    #pprint(serverStatusResult)
     db=client.Brwindy
+    if(db.Posts2.find({'Nome':obj['Name']}).count() == 1 ):
+        return False
     result=db.Posts2.insert_one(obj)
     pprint(result)
 
 
-CsvToDict(pathArquivo="../DadosOriginais/Dados - Boias - Marinha - Qualificados - 2018-05-07/fortaleza.csv", NomeBoia="fortaleza")
+
+
+def AdicionarBoiasPasta(path):
+#CsvToDict(pathArquivo="../DadosOriginais/Dados - Boias - Marinha - Qualificados - 2018-05-07/fortaleza.csv", NomeBoia="fortaleza")
+   # path = "../DadosOriginais/Dados - Boias - Marinha - Qualificados - 2018-05-07"
+    datadir = os.listdir(path)
+    print(datadir)
+
+    for boia in datadir:
+        nome = boia.replace(".csv","")
+        ret = CsvToDict(pathArquivo = path+'/'+boia, NomeBoia= nome)
+        if(ret == False ):
+            print("Error Na boia " + nome )
+
+AdicionarBoiasPasta(path="../DadosOriginais/Dados - Boias - Marinha - Qualificados - 2018-05-07")
