c40ab6948e4fe694cd290cb6f53d7c8aa811cbdc
Rafael Bessa <rafablou@gmail.com>
2019-11-01 11:49:20 -0300

    Endpoints para pegar nome de todas as boias e dados pelo nome da boia, [/boiasNomes,/boia/<boiaNome>]

--- python_rest/server.py
+++ python_rest/server.py
@@ -3,11 +3,11 @@ from flask_restful import Resource, Api
 #from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
-
+from pymongo import MongoClient
 #db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
-
+from bson.json_util import dumps
 boias = [{"id":"001","Nome":"aaa","Pontos":[(0,1),(2,1)] },{"id":"002","Nome":"bbb","Pontos":[(0,1),(2,1)]} ]
 
 def FirstOrDefault(sequence, id):
@@ -17,22 +17,43 @@ def FirstOrDefault(sequence, id):
     return None
 
 
-
-#Retorna o dicionario de uma boia especifica recebendo o id dela
+#Retorna o dicionario de uma boia especifica recebendo o nome dela endpoint: /boia/nome
 class Boia_id(Resource):
-    def get(self, boiaId):
-        result = FirstOrDefault(boias,boiaId)
-        print(result)
-        return jsonify(result)
+    def get(self, boiaNome):
+        client = MongoClient("mongodb+srv://Bessa:vEiz8yoTtgOZzhyC@brwindy-adtf0.mongodb.net/test?retryWrites=true&w=majority")    
+        db=client.Brwindy
+        data = db.Posts2.find({'Name':boiaNome}) 
+        return jsonify(dumps(data))
 
-#Retorna todas as boias
+#Retorna todas as boias /boias
 class Boias(Resource):
     def get(self):
-        return jsonify(boias)
+        client = MongoClient("mongodb+srv://Bessa:vEiz8yoTtgOZzhyC@brwindy-adtf0.mongodb.net/test?retryWrites=true&w=majority")    
+        db=client.Brwindy
+        data = db.Posts2.find() 
+        blist = []
+        for b in data:
+            blist.append(dumps(b))
+        #return jsonify(data)
+        return jsonify(blist)
 
 
-api.add_resource(Boia_id, '/boia/<boiaId>')
+
+#Retorna o nome de todas as boias /boiasNomes
+class BoiasNome(Resource):
+    def get(self):
+        client = MongoClient("mongodb+srv://Bessa:vEiz8yoTtgOZzhyC@brwindy-adtf0.mongodb.net/test?retryWrites=true&w=majority")    
+        db=client.Brwindy
+        data = db.Posts2.find({},{'Name':1}) 
+        blist = []
+        for b in data:
+            blist.append((b['Name']))
+        #return jsonify(data)
+        return jsonify(blist)
+
+
+api.add_resource(Boia_id, '/boia/<boiaNome>')
 api.add_resource(Boias, '/boias')
-
+api.add_resource(BoiasNome, '/boiasNomes')
 if __name__ == '__main__':
-     app.run(port='5002')
+     app.run(port='5002', debug=True)
