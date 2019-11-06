ecd65f685856b9e80acd29eaa10c22d870b3ab70
Rafael Bessa <rafablou@gmail.com>
2019-11-06 11:00:54 -0300

    Querys complexas mongo/ Documentacao do endPoint

---
+++ python_rest/EndPointsReadMe.txt
@@ -0,0 +1,21 @@
+-- Retornar o nome de todas as boias 
+'/boiasNomes'
+
+-- Retornar todos os dados de todas as boias
+'/boias'
+
+-- Retornar uma boia especifica, passando o nome como parametro
+'/boia/<boiaNome>'
+
+-- Retornar uma lista de anos cadastrados
+'boiasAno'
+
+--Retorna uma lista com todas as categorias de dados (ex WindSpeed, WindDirection)
+'boiasCat'
+
+-- Retorna lista com atributos especificos do objeto
+'boia/<boiaNome>/<Cat>'
+
+-- Retorna valores apartir de operacao (maior, menor, igual)
+'boia/<boiaNome>/<Cat>/<operacao>/<valor>'
+
---
+++ python_rest/TestDb.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+#from sqlalchemy import create_engine
+from json import dumps
+from flask_jsonpify import jsonify
+from pymongo import MongoClient
+#db_connect = create_engine('sqlite:///chinook.db')
+from pprint import pprint
+from bson.json_util import dumps
+
+
+client = MongoClient("mongodb+srv://Bessa:vEiz8yoTtgOZzhyC@brwindy-adtf0.mongodb.net/test?retryWrites=true&w=majority")    
+db=client.Brwindy
+data = db.PostsTest.find({'Data.Dado.Date.Year': '2016'},{'Data.Dado.Date.Year':1} )
+#data = db.inventory.find({'Data.Dado': {"$elemMatch" : {"Wspd" : "4,237735462" }}})
+for d in data:
+    print(d)
+pprint(data)
